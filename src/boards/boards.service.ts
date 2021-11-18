45be84dc854f0990275774f9bcbbac6ea7f37d04
KangJuO <juy5437@gmail.com>
2021-11-18 20:47:06 +0900

    [add]: createBoard method 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +9,18 @@ export class BoardsService {
   //모든 게시글을 가져오는 메소드
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      //원래는 디비가 자동 생성해주지만 지금은 로컬에서 테스트할거야 랜덤아이디 생성용 uuid임
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
