9b18e4d3d1eb69b20140ff59cf1f2ffcbffe165d
KangJuO <juy5437@gmail.com>
2021-11-23 00:05:20 +0900

    [mod]: custom pipe-check validation of board type

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,10 +1,25 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log('value', value);
-    console.log('metadata', metadata);
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
 
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOption.indexOf(status);
+    return index !== -1;
   }
 }
